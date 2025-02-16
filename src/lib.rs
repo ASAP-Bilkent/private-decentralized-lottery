e352a9e2e41850f9131a5954864e793f6247a57a
Ege <egeaybars1102@gmail.com>
2025-02-16 23:13:39 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -56,3 +56,29 @@ pub async fn request_vrf(commitment: H256) -> Result<(), Box<dyn std::error::Err
 
     Ok(())
 }
+
+pub async fn get_random_number(commitment: H256) -> Result<U256, Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let contract_address = "0xc81228e835DDef6E0F08D91bC714213B371Ad4c3".parse::<Address>()?;
+    let priv_key = env::var("PRIVATE_KEY").unwrap();
+    let rpc_url = env::var("RPC_URL").unwrap();
+    let provider = Provider::<Http>::try_from(rpc_url)?;
+
+    let wallet = priv_key
+        .parse::<LocalWallet>()?
+        .with_chain_id(Chain::Sepolia);
+    let client = SignerMiddleware::new(provider.clone(), wallet.clone());
+
+    let abi_json = fs::read_to_string("../abi.json")?;
+    let abi: Abi = serde_json::from_str(&abi_json)?;
+    let contract = Contract::new(contract_address, abi, Arc::new(client));
+
+    // Call the commitmentRandomness mapping
+    let random_number: U256 = contract
+        .method::<_, U256>("commitmentRandomness", commitment)?
+        .call()
+        .await?;
+
+    Ok(random_number)
+}
